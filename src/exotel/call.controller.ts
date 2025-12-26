6f0af9da9a61dc48ba6803bd339a2b8487c50221
lkmali <laxmanasawa9323@gmail.com>
2025-12-26 20:49:47 +0530

    added query

--- src/exotel/call.controller.ts
+++ src/exotel/call.controller.ts
@@ -39,6 +39,23 @@ router.post("/call", async (req, res) => {
   }
 })
 
+router.post("/call-information", async (req, res) => {
+  const { from } = req.body
+  console.log("post-->call-information-->body", req.body)
+
+  console.log("post-->headers", req.headers)
+  res.json({ ok: true, message: "Call information received" })
+})
+
+router.get("/call-information", async (req, res) => {
+  const query = req.query
+  const headers = req.headers
+  console.log("GET-->From", query)
+
+  console.log("GET-->headers", headers)
+  res.json({ ok: true, message: "Call information received" })
+})
+
 /**
  * EXOML endpoint — Exotel Flow v1 Passthru calls this.
  * This EXOML tells Exotel to start streaming audio to your WebSocket.
@@ -46,9 +63,7 @@ router.post("/call", async (req, res) => {
 router.get("/exoml/ai", (req, res) => {
   console.log("Exotel requested EXOML", req.headers)
   const wsUrl = `wss://${PUBLIC_HOST}${EXOTEL_STREAM_PATH}?sample-rate=8000`
-
   logger.info("Exotel Voicebot requested WSS", wsUrl)
-
   res.json({
     url: wsUrl,
   })
